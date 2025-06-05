e616aa2e1783ce55c715f26f0e104518e18d6874
José Cerrato <108470207+Ashe675@users.noreply.github.com>
2025-06-04 23:28:08 -0600

    fix: permission denied

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "🔧 Ajustando permisos..."
-chown -R www-data:www-data /var/www/html/public/storage
-chmod -R 755 /var/www/html/public/storage
+chown -R www-data:www-data /var/www/html/storage/app/public/
+chmod -R 755 /var/www/html/storage/app/public/
 
 echo "🚀 Iniciando Apache..."
 exec apache2-foreground
